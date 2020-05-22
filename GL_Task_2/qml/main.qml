f4a0b90c45a3cbccea3aed4a1b64ac8027065afe
Artem Dmytriv <artemdmytriv@gmail.com>
2020-05-22 18:46:47 +0300

    test qml, test cpuinfo

---
+++ GL_Task_2/qml/main.qml
@@ -0,0 +1,9 @@
+import QtQuick 2.12
+import QtQuick.Window 2.12
+
+Window {
+    visible: true
+    width: 600
+    height: 400
+    title: qsTr("Loadability monitor")
+}
--- GL_Task_2/src/main.cpp
+++ GL_Task_2/src/main.cpp
@@ -1,29 +1,43 @@
 #include "include/head.h"
-#include "include/widget.h"
+#include "UtilClass.h"
+#include "cpuinfo.h"
+#include "raminfo.h"
 
-#include <QApplication>
 
-CPUCounter cpu;
-RAMCounter ram;
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    //   QApplication a(argc, argv);
-    //    Widget w;
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QGuiApplication app(argc, argv);
+    QQmlApplicationEngine engine;
 
-    int i = 10;
+//    int i = 5;
+//    cout << "RAM total\t" << ram.getTotalMB() << endl;
 
-    cout << "RAM total\t" << ram.getTotalMB() << endl;
+//    while (i--){
+//        cout << "CPU: "<< cpu.getUsage() << endl;
+//        cout << "RAM: "<< ram.getUsage() << endl;
+//        Sleep(1000);
+//    }
 
-    while (i--){
-        cout << "CPU: "<< cpu.getUsage() << endl;
-        cout << "RAM: "<< ram.getUsage() << endl;
-        Sleep(1000);
-    }
-    //    w.show();
-    //    return a.exec();
-    return  1;
+    CPUInfo * cpu = new CPUInfo;
+
+    cout << cpu->getThreads() << endl;
+    cout << cpu->getArch() << endl;
+    cout << cpu->getCpuName() << endl;
+
+    const QUrl url(QStringLiteral("qrc:/qml/main.qml"));
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+                     &app, [url](QObject *obj, const QUrl &objUrl) {
+                        if (!obj && url == objUrl)
+                            QCoreApplication::exit(-1);
+                    }, Qt::QueuedConnection);
+    engine.load(url);
+
+    return app.exec();
 }
 
